82519791e4fc8c0d94f2dcd1206dfb3db88a0f62
Tatiane Pedrelli <tatiane@tatianepedrelli.com.br>
2020-12-10 22:29:40 -0300

    Instalado pacotes

---
+++ src/App.tsx
@@ -0,0 +1,27 @@
+import React from 'react'
+
+const App = () => (
+    <>
+    <header>
+        <nav>
+            <ul>
+                <li>HOME</li>
+                <li>ABOUT US</li>
+                <li>OFFER TOOL</li>
+                <li>RENT TOOL</li>
+            </ul>
+        </nav>
+    </header>
+    <main>
+        <a href="tatianepedrelli.com.br">TATI</a>
+        <a href="silvanoschroder.com.br">SIL</a>
+    </main>
+    <section></section>
+    <article></article>
+    <footer>
+        <p>&#169; 2000—2020 Tatiane Pedrelli. Todos os direitos reservados. Uso &#38; Privacidade.</p>
+    </footer>
+    </>
+)
+
+export default App
